687d33946183fd4815f2fd0e5aa4719fd692a7c9
carles espunya <carlespuna@gmail.com>
2024-03-14 20:05:03 +0100

    Done

--- src/core/domain/habit/progress/progress.ts
+++ src/core/domain/habit/progress/progress.ts
@@ -7,7 +7,7 @@ export class Progress {
     readonly observations: string,
   ) {}
 
-  static create(id, date: Date, observations: string): Progress {
+  static create(id: string, date: Date, observations: string): Progress {
     if (date > new Date()) throw InvalidProgressError.withDate(date)
 
     return new Progress(id, date, observations)
---
+++ src/core/test/habit/progress.mother.ts
@@ -0,0 +1,26 @@
+import { Progress } from '../..//domain/habit/progress/progress'
+import { v4 as uuidv4 } from 'uuid'
+
+export class ProgressMother {
+  private id = uuidv4()
+  private date = new Date()
+  private observations = 'observations'
+
+  build(): Progress {
+    return Progress.create(this.id, this.date, this.observations)
+  }
+
+  static create(): Progress {
+    return new ProgressMother().build()
+  }
+
+  withId(id: string) {
+    this.id = id
+    return this
+  }
+
+  withDate(date: Date) {
+    this.date = date
+    return this
+  }
+}
